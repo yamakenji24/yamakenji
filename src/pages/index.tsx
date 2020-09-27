d877e3678b50ee0188fc857f71dc0813ce8334ea
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-09-28 03:07:30 +0900

    set components for Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Head from 'next/head';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Header />
       <div>Home</div>
     </div>
   );
