f5b67f094f37e63ebeb000d0ec975fd6c428c00e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-09-28 03:06:36 +0900

    set files for SSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,33 @@
+import * as React from 'react';
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
+import '../../styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
