d674f85843a302c7d58119b1237e18dee5447f80
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-09-29 21:44:06 +0900

    add info to top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import * as React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Typography } from '@material-ui/core';
+import Profile from '../components/top/profile';
+import Experience from '../components/top/experience';
 
-export default function Home() {
+const Home = () => {
+  const classes = sectionStyles();
+
   return (
-    <div>
-      <div>Home</div>
+    <div className={classes.main}>
+      <section className={classes.section}>
+        <Container maxWidth="md" className="container">
+          <div className="sectionHeader">
+            <Typography variant="h4">About me</Typography>
+          </div>
+          <div className="sectionBody">
+            <Profile />
+          </div>
+        </Container>
+      </section>
+
+      <section className={classes.section}>
+        <Container maxWidth="md" className="container">
+          <div className="sectionHeader">
+            <Typography variant="h4">Experience</Typography>
+          </div>
+          <div className="sectionBody">
+            <Experience />
+          </div>
+        </Container>
+      </section>
     </div>
   );
-}
+};
+
+const sectionStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      '& section': {
+        padding: `${theme.spacing(10)}px 0`,
+      },
+    },
+    section: {
+      '& > .container': {
+        display: 'flex',
+        flexDirection: 'row',
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column',
+          '& > .sectionHeader': {
+            paddingBottom: theme.spacing(4),
+          },
+        },
+        '& > .sectionHeader': {
+          flex: 1,
+          '& > .sectionTitle': {
+            fontWeight: 700,
+            letterSpacing: theme.spacing(0.5),
+          },
+        },
+        '& > .sectionBody': {
+          flex: 2,
+        },
+      },
+    },
+  }),
+);
+
+export default Home;
