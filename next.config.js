69b5daa3418e8ecb1b4ea7d79ee63f3e0d169902
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-02-20 03:39:07 +0900

    env on work_url

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     ACCESS_KEY: process.env.ACCESS_KEY,
     X_API_KEY: process.env.X_API_KEY,
     ARTICLE_URL: process.env.ARTICLE_URL,
+    WORK_URL: process.env.WORK_URL,
   },
   images: {
     domains: ['images.microcms-assets.io'],
