8fedbe0c761ae8aedda180d96d376ffb90536b91
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-02-19 23:50:40 +0900

    add cms domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   env: {
     ACCESS_KEY: process.env.ACCESS_KEY,
+    X_API_KEY: process.env.X_API_KEY,
+    ARTICLE_URL: process.env.ARTICLE_URL,
+  },
+  images: {
+    domains: ['images.microcms-assets.io'],
   },
 };
