8fcfeef4ec32de989bfb412e5090c7786ab10ee0
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-02-22 04:15:51 +0900

    feat/layout on top image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { TopLayout } from '../ui/top/topLayout';
+import { TopImage } from '../ui/top/topImage';
 
 const Home = () => {
   const classes = sectionStyles();
 
   return (
     <div className={classes.main}>
+      <TopImage />
       <TopLayout tag="Profile" />
       <TopLayout tag="Experience" />
     </div>
@@ -20,7 +22,7 @@ const sectionStyles = makeStyles((theme: Theme) =>
         padding: `${theme.spacing(10)}px 0`,
       },
     },
-  }),
+  })
 );
 
 export default Home;
