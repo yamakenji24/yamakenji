eed0044646eca930e65a423b50d6308c43f7fc93
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-10-12 00:51:47 +0900

    WIP/jest not working

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src'],
+  preset: "ts-jest",
+  roots: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.jest.json',
     },
   },
-
   moduleDirectories: ['node_modules', '<rootDir>'],
   testPathIgnorePatterns: [
     '/node_modules/',
     '<rootDir>/src/__tests__/utils',
+    '<rootDir>/src/__tests__/data',
   ],
 }
