6f758b4e35c1fb2f13ed208e7e0dfe6462c7975d
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-10-15 18:30:20 +0900

    feat/Layout component for meta and header

---
+++ src/ui/common/Layout.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { Box } from '@chakra-ui/react';
+import Head from 'next/head';
+import { Header } from 'ui/common/header';
+
+const DEFAULT_TITLE='yamakenji24 profile';
+const DEFAULT_DESCRIPTION='Profile for yamakenji24';
+
+interface Props {
+  ogImageUrl?: string;
+}
+
+export const Layout: FC<Props> = ({children, ogImageUrl}) => {
+  return (
+    <Box>
+      <Head>
+        <title>{DEFAULT_TITLE}</title>
+        <link rel="icon" href="/favicon.png" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={DEFAULT_TITLE} />
+        <meta property="og:title" content={DEFAULT_TITLE} />
+        <meta property="og:description" content={DEFAULT_DESCRIPTION} />
+        <meta property="og:image" content={ogImageUrl} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={DEFAULT_TITLE} />
+        <meta name="twitter:description" content={DEFAULT_DESCRIPTION} />
+        <meta name="twitter:image " content={ogImageUrl} />
+
+      </Head>
+      <Header />
+      <main>
+        <Box>{children}</Box>
+      </main>
+    </Box>
+  )
+}
