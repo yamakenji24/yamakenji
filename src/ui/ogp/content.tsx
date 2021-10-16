dd16a74fdd67f6c0e719e1b30dd7b1d38da17a3e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-10-16 19:27:53 +0900

    feat/changed design for ogp

--- src/ui/ogp/content.tsx
+++ src/ui/ogp/content.tsx
@@ -4,45 +4,71 @@ export const OGPContent = (props): JSX.Element => (
       <style>{styles}</style>
     </head>
     <body>
-      <h1>yamakenji24 profile</h1>
-      <h2>{props.title}</h2>
-      <div className="logo">
-        <img
-          src="https://yamakenji.vercel.app/logo.png"
-          width="100"
-          height="100"
-        />
-        <h3>yamakenji24</h3>
+      <div>
+        <div className="card-box">
+          <img src="https://yamakenji.vercel.app/logo.png" width="200px" height="200px" />
+
+          <div className="profile">
+            <p className="key-title">Detail</p>
+            <ProfileData tag="name" name="Kenji Yamashita" />
+            <ProfileData tag="id" name="@yamakenji24" />
+
+            <p className="key-title">Site</p>
+            <ProfileData tag="Website" name="https://yamakenji.vercel.app" />
+            <ProfileData tag="Where" name={props.title} />
+
+            <p className="key-title">Social</p>
+            <ProfileData tag="github" name="yamakenji24" />
+            <ProfileData tag="zenn" name="yamakenji24" />
+          </div>
+        </div>
       </div>
     </body>
   </html>
 );
 
+const ProfileData = ({ tag, name }: { tag: string; name: string }) => (
+  <div className="key-detail">
+    <p className="key">{tag}</p>
+    <p className="detail">{name}</p>
+  </div>
+);
+
 const styles = `
   html, body {
     height: 100%;
-    display: grid;
-    color: #696969;
-    background: #fffafa;
-    border: double 8px #20b2aa;
+    background-color: #222222;
   }
-  .logo {
-    margin: auto;
+  .card-box {
     display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    margin: auto;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    color: white;
   }
-  h1 {
-    margin: auto auto 0 auto;
-    font-size: xxx-large;
-    color: black;
+  .profile {
+    padding-left: 8rem;
   }
-  h2 {
-    margin: auto auto 0 auto;
-    font-size: xx-large;
-    color: black;
+  .key-detail {
+    display: flex;
+    font-size: 1rem;
+    font-weight: bold;
+    line-height: 0;
   }
-  h3 {
-    margin: 0 auto 0 auto;
-    font-size: xxx-large;
-    color: black;
+  .key-title {
+    color: #FF00FF
+  }
+  .key {
+    color: #00CCFF;
+    margin-right: 1rem;
+  }
+  .detail {
+    color: #FFFF33;
   }
 `;
