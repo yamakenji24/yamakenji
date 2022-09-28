b424fd5e57cabe271eafac218349fd52948ace7a
yamakenji24 <45092341+yamakenji24@users.noreply.github.com>
2022-09-29 00:37:33 +0900

    Update/update typescript (#43)
    * update ts to v4.8.4

--- src/__tests__/page/contact.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '../utils';
-
-import Contact from 'pages/contact';
-
-test('/contact', () => {
-  const { container } = render(<Contact />);
-  expect(container).toMatchSnapshot();
-});
