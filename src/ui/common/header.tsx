7f373e6e373e07be6fc484feabf4dc7010e408c1
yamakenji24 <yamakenji24@gmail.com>
2023-07-01 18:32:05 +0900

    chakra-uiからpandaへ移行

--- src/ui/common/header.tsx
+++ src/ui/common/header.tsx
@@ -1,30 +1,25 @@
-import { Flex, IconButton, useColorMode } from '@chakra-ui/react';
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image';
 import { NavBar } from './navBar';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { css } from '../../../styled-system/css';
 
 export const Header = (): JSX.Element => {
-  const { colorMode, toggleColorMode } = useColorMode();
-
   return (
-    <Flex as="nav" px={4} py={2} bg="#222222" justifyContent={'space-between'}>
+    <nav
+      className={css({
+        display: 'flex',
+        padding: '4px 16px',
+        bg: '#222222',
+        justifyContent: 'space-between',
+      })}
+    >
       <Logo />
-      <Flex align="center">
-        <NavBar />
-        <IconButton
-          size="sm"
-          my="auto"
-          aria-label="DarkMode Switch"
-          icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-          onClick={toggleColorMode}
-        />
-      </Flex>
-    </Flex>
+      <NavBar />
+    </nav>
   );
 };
 
 const Logo = (): JSX.Element => (
-  <Flex pl={['2', '4']} pr={['4', '8']}>
+  <div className={css({ display: 'flex' })}>
     <Image src="/logo.png" width="80" height="60" alt="" />
-  </Flex>
+  </div>
 );
