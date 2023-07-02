599c4c15a8b22e707e98a328c2b21237bcee27a8
yamakenji24 <yamakenji24@gmail.com>
2023-07-02 18:28:06 +0900

    light mode

--- src/ui/top/GitHubActivity.tsx
+++ src/ui/top/GitHubActivity.tsx
@@ -9,6 +9,6 @@ const GitHubCalendar = dynamic(() => import('react-github-calendar'));
 export const GitHubActivity = () => (
   <div className={css({ padding: '0 24px' })}>
     <Title title="Days I Code" fontSize="h4" />
-    <GitHubCalendar username="yamakenji24" blockSize={15} fontSize={16} />
+    <GitHubCalendar username="yamakenji24" blockSize={15} fontSize={16} colorScheme='light'/>
   </div>
 );
