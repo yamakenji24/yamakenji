72d4a046bbf0adf6060ae3e3c66cab42348fc33f
yamakenji24 <yamakenji24@gmail.com>
2023-07-02 16:10:10 +0900

    profileコンポーネントをpandaへ移行

--- src/ui/top/index.ts
+++ src/ui/top/index.ts
@@ -1,27 +1,3 @@
 export { Profile } from './profile';
 export { Experience } from './experience';
 export { Skills } from './skill';
-
-export interface Services {
-  service: string;
-  url: string;
-}
-
-export const services: Array<Services> = [
-  {
-    service: '/github.svg',
-    url: 'https://github.com/yamakenji24',
-  },
-  {
-    service: '/hatenablog.svg',
-    url: 'https://yamakenji24.hatenablog.com/',
-  },
-  {
-    service: '/qiita.png',
-    url: 'https://qiita.com/yamakenji',
-  },
-  {
-    service: '/zenn.svg',
-    url: 'https://zenn.dev/yamakenji24',
-  },
-];
