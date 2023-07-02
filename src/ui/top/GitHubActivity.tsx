ba04dec6a1c8168c994a427189c1ef73bb1a0db2
yamakenji24 <yamakenji24@gmail.com>
2023-07-02 18:17:42 +0900

    fix github calendar

--- src/ui/top/GitHubActivity.tsx
+++ src/ui/top/GitHubActivity.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { Tooltip } from 'react-tooltip';
 import { Title } from 'ui/common/title';
 import { css } from '../../../styled-system/css';
 
@@ -10,8 +9,6 @@ const GitHubCalendar = dynamic(() => import('react-github-calendar'));
 export const GitHubActivity = () => (
   <div className={css({ padding: '0 24px' })}>
     <Title title="Days I Code" fontSize="h4" />
-    <GitHubCalendar username="yamakenji24" blockSize={15} fontSize={16}>
-      <Tooltip />
-    </GitHubCalendar>
+    <GitHubCalendar username="yamakenji24" blockSize={15} fontSize={16} />
   </div>
 );
